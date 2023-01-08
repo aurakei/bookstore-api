996fbd3f3c2f097769e6f073048efce5f3b777f7
aurakei <aurakeith@gmail.com>
2023-01-08 17:15:59 +0300

    card styling

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Card() {
   return (
     <div className='card'>
-        <img src="./images/bg3.jpg" alt="" />
+        <img src="./images/bg2.jpg" alt="" />
         <div className="bottom">
             <h3 className="title">React JS</h3>
             <p className="amount">&#8377;3290</p>
