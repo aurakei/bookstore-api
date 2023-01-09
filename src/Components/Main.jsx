c945a5c52023f0c40ad4bf7e398f6849bb5932eb
aurakei <aurakeith@gmail.com>
2023-01-09 12:41:40 +0300

    adding axios

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, {useState} from 'react';
 import Card from './Card';
 import { FaBeer, FaSearch } from 'react-icons/fa';
+import axios from "axios";
 
 export default function Main() {
+  const [search, setSearch]=useState("");
+  const searchBook=(evt)=>{
+    if(evt.key==="Enter"){
+      console.log("Hello")
+    }
+  }
   return (
     <>
     <div className="header">
@@ -13,13 +20,20 @@ export default function Main() {
             <h2>Find Your Soul</h2>
             <FaBeer />
             <div className="search">
-                <input type="text" placeholder='Fill in your Book name' />
+                <input type="text" placeholder='Fill in your Book name'
+                value={search} onChange={e=>setSearch(e.target.value)}
+                onKeyPress={searchBook} />
                 <button><FaSearch /></button>
             </div>
             <img src="./images/bg4.jpg" alt="" />
         </div>
     </div>
     <div className="container">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+      <Card />
       <Card />
     </div>
     </>
